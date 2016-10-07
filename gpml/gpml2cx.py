a22c23af59a73ed66ce275d5c984cfd647470c9b
Dexter Pratt <dexterpratt.bio@gmail.com>
2016-10-06 21:17:42 -0700

    sync gpml changes

---
+++ gpml/gpml2cx.py
@@ -0,0 +1,8 @@
+import xml.etree.ElementTree as ET
+path = "/Users/dexter/Downloads/WP3895_89784.gpml"
+tree = ET.parse(path)
+root = tree.getroot()
+for child in root:
+    print child.tag, child.attrib
+    for grandchild in child:
+        print grandchild.tag, grandchild.attrib
