393f7c700b0d8d85e712f4165430a68c94f5514a
chenjing <jingjingbic@gmail.com>
2017-03-01 15:47:26 -0800

    Fixed rm_duplicate_rows.py and moved it to the rood dir of this package.

---
+++ rm_duplicate_rows.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import sys
+
+
+def main():
+
+    lines = set()
+    firstLine = True
+
+    for line in sys.stdin:
+        if firstLine:
+            sys.stdout.write(line)
+            firstLine = False
+            continue
+        if line not in lines:
+            lines.add(line)
+            sys.stdout.write(line)
+
+
+if __name__ == '__main__':
+    main()
