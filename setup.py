359155cdf2e8b76e17d7c7197a425e95dd06543e
Chris Churas <churas.camera@gmail.com>
2019-01-11 09:46:00 -0800

    cleaned up a bit and updated to pull version from __init__.py and to use the readme/history for the long description

--- setup.py
+++ setup.py
@@ -8,79 +8,81 @@ https://github.com/pypa/sampleproject
 from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
-from os import path
+import os
+import re
 
-here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the relevant file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
 
-if __name__ == '__main__':
-    setup(
-        name='ndexutil',
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
 
-        # Versions should comply with PEP440.  For a discussion on single-sourcing
-        # the version across setup.py and the project code, see
-        # https://packaging.python.org/en/latest/single_source_version.html
-        version='0.0.30',
+with open(os.path.join('ndexutil', '__init__.py')) as ver_file:
+    for line in ver_file:
+        if line.startswith('__version__'):
+            version=re.sub("'", "", line[line.index("'"):])
 
-        description='NDEx utils',
-        long_description='Unsupported NDEx utilities',
 
-        # The project's main homepage.
-        url='https://github.com/ndexbio/ndexutils',
+setup(
+    name='ndexutil',
+    version=version,
+    description='Unsupported NDEx utilities',
+    long_description=readme + '\n\n' + history,
 
-        # Author details
-        author='The NDEx Project',
-        author_email='contact@ndexbio.org',
+    # The project's main homepage.
+    url='https://github.com/ndexbio/ndexutils',
 
-        # Choose your license
-        license='BSD',
+    # Author details
+    author='The NDEx Project',
+    author_email='contact@ndexbio.org',
 
-        # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        classifiers=[
-            # How mature is this project? Common values are
-            #   3 - Alpha
-            #   4 - Beta
-            #   5 - Production/Stable
-            'Development Status :: 4 - Beta',
+    # Choose your license
+    license='BSD',
 
-            # Indicate who your project is intended for
-            'Intended Audience :: Science/Research',
-            'Topic :: Scientific/Engineering :: Information Analysis',
-            'Topic :: Scientific/Engineering :: Bio-Informatics',
-            'Topic :: Scientific/Engineering :: Medical Science Apps.',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
 
-            # Pick your license as you wish (should match "license" above)
-            'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
 
-            # Specify the Python versions you support here. In particular, ensure
-            # that you indicate whether you support Python 2, Python 3 or both.
-            'Programming Language :: Python :: 2.7',
-        ],
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: BSD License',
 
-        # What does your project relate to?
-        keywords='network analysis biology',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6'
+    ],
 
-        # You can just specify the packages manually here if your project is
-        # simple. Or you can use find_packages().
-        packages=find_packages(exclude=[]),
-        data_files=[('schema', ['ndexutil/tsv/loading_plan_schema.json'])],
-        install_requires = [
-            'ndex2',
-            'requests',
-            'requests_toolbelt',
-            'networkx==1.11',
-            'urllib3>=1.16',
-            'pandas',
-            'enum34',
-            'enum; python_version == "2.6" or python_version=="2.7"',
-            'jsonschema'
-        ],
+    # What does your project relate to?
+    keywords='network analysis biology',
 
-        include_package_data=True
-    )
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=[]),
+    data_files=[('schema', ['ndexutil/tsv/loading_plan_schema.json'])],
+    install_requires = [
+        'ndex2',
+        'requests',
+        'requests_toolbelt',
+        'networkx==1.11',
+        'urllib3>=1.16',
+        'pandas',
+        'enum34',
+        'enum; python_version == "2.6" or python_version=="2.7"',
+        'jsonschema'
+    ],
+    include_package_data=True
+)
 
 
 
