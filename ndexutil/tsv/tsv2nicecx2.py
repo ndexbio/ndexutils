979a7f2de2a900f7f0ed2986d5e7b388c79a6245
Chris Churas <churas.camera@gmail.com>
2019-01-11 14:38:54 -0800

    added ContentImporter class

--- ndexutil/tsv/tsv2nicecx2.py
+++ ndexutil/tsv/tsv2nicecx2.py
@@ -1,15 +1,122 @@
 
 import json
+import os
 from os import path
-from jsonschema import validate
+import jsonschema
 import pandas as pd
-from ndex2cx.nice_cx_builder import NiceCXBuilder
 import time
 import re
+import logging
+from ndex2cx.nice_cx_builder import NiceCXBuilder
+from ndex2.client import Ndex2
 
 version="0.1"
 
 node_id_lookup = {}
+
+logger = logging.getLogger(__name__)
+
+
+class ContentImporter(object):
+    def __init__(self, server, username, password, **attr):
+        self.update_mapping = {}
+        self.server = server
+        self.username = username
+        self.password = password
+        self.network = None
+        self.ndex = Ndex2(self.server, self.username, self.password)
+
+        networks = self.ndex.get_network_summaries_for_user(self.username)
+        for nk in networks:
+            if nk.get('name') is not None:
+                self.update_mapping[nk.get('name').upper()] = nk.get('externalId')
+
+    def process_file(self, file_name, load_plan_path, name, style_template=None, custom_header=None, delimiter='\t'):
+        # ==============================
+        # LOAD TSV FILE INTO DATAFRAME
+        # ==============================
+        if not os.path.isfile(file_name): # If file is not in main directory try the ./data directory
+            file_name = os.path.join('data', file_name)
+
+        if not os.path.isfile(load_plan_path): # If file is not in main directory try the ./data directory
+            load_plan_path = os.path.join('data', load_plan_path)
+
+        with open(file_name, 'r', encoding='utf-8', errors='ignore') as tsvfile:
+            if custom_header is None:
+                header = [h.strip() for h in tsvfile.readline().split(delimiter)]
+
+                df = pd.read_csv(tsvfile, delimiter=delimiter, na_filter=False, engine='python', names=header,
+                                 dtype=str, error_bad_lines=False, comment='#')
+            else:
+                if isinstance(custom_header, list):
+                    df = pd.read_csv(tsvfile, delimiter=delimiter, na_filter=False, engine='python', names=custom_header,
+                                     dtype=str, error_bad_lines=False, comment='#')
+                else:
+                    raise Exception('Custom header provided was not of type list')
+
+        # =====================
+        # LOAD TSV LOAD PLAN
+        # =====================
+        if load_plan_path is not None:
+            try:
+                with open(load_plan_path, 'r') as lp:
+                    load_plan = json.load(lp)
+            except jsonschema.ValidationError as e1:
+                logger.exception("Failed to parse the loading plan: " + e1.message)
+                logger.error('at path: ' + str(e1.absolute_path))
+                logger.error("in block: ")
+                logger(e1.instance)
+        else:
+            raise Exception('Please provide a load plan')
+
+        # ====================
+        # UPPERCASE COLUMNS
+        # ====================
+        rename = {}
+        for column_name in df.columns:
+            rename[column_name] = column_name.upper()
+
+        network = convert_pandas_to_nice_cx_with_load_plan(df, load_plan)
+        network.set_name(name)
+        if style_template is not None:
+            logger.debug('Applying style from network: ' + style_template)
+            network.apply_template(username=self.username, password=self.password, server=self.server,
+                               uuid=style_template)
+
+        self.network = network
+
+    def upload_network(self, re_use_metadata=True):
+        network_update_key = self.update_mapping.get(self.network.get_name().upper())
+        if network_update_key is not None and re_use_metadata in ['true', 'True', 'yes', True]:
+            logger.debug("Updating")
+            self.update_network_properties(network_update_key)
+            message = self.network.update_to(network_update_key, self.server, self.username, self.password)
+        else:
+            logger.debug("New network")
+            message = self.network.upload_to(self.server, self.username, self.password)
+
+        logger.info(message)
+
+    def get_network_properties(self, uuid):
+
+        network_properties_stream = self.ndex.get_network_aspect_as_cx_stream(uuid, 'networkAttributes')
+
+        network_properties = network_properties_stream.json()
+        return_properties = {}
+        for net_prop in network_properties:
+            return_properties[net_prop.get('n')] = net_prop.get('v')
+
+        return return_properties
+
+    def update_network_properties(self, uuid):
+        network_properties = self.get_network_properties(uuid)
+
+        for k, v in network_properties.items():
+            self.network.set_network_attribute(k, v)
+
+    def print_summary(self):
+        print('Loading...')
+
 
 #=====================
 # NON-CLASS FUNCTIONS
@@ -23,7 +130,7 @@ def convert_pandas_to_nice_cx_with_load_plan(pandas_dataframe, load_plan, max_ro
     with open(path.join(here, 'loading_plan_schema.json')) as json_file:
         plan_schema = json.load(json_file)
 
-    validate(load_plan, plan_schema)
+    jsonschema.validate(load_plan, plan_schema)
 
     node_lookup = {}
     nice_cx_builder = NiceCXBuilder()
