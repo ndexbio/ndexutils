b9712b14f8ed27a5030074a9e10ef52f9e4c6bf7
Chris Churas <churas.camera@gmail.com>
2019-02-12 14:44:37 -0800

    new version

---
+++ tests/test_ndexutils.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `ndexutils` script."""
+
+import tempfile
+import unittest
+
+from ndexutil import ndexutils
+
+
+class TestNDexUtils(unittest.TestCase):
+    """
+    Tests ndexutils.py
+    """
+    def setUp(self):
+        """Set up test fixtures, if any."""
+        pass
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+        pass
+
+    def test_parse_arguments(self):
+        res = ndexutils._parse_arguments('hi', [])
+        self.assertEqual(res.command, None)
