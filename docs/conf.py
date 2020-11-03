c882ca47d2412d48f95f6f175bfe4b9a2f0912e2
Chris Churas <churas.camera@gmail.com>
2020-11-03 11:45:30 -0800

    added intersphinx documentation to properly link to ndex2

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,9 @@ import ndexutil
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -77,6 +79,12 @@ pygments_style = 'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
+
+# Add mappings
+intersphinx_mapping = {
+    'ndex2': ('http://ndex2.readthedocs.io/en/latest', None),
+    'python': ('http://docs.python.org/3', None),
+}
 
 
 # -- Options for HTML output -------------------------------------------
